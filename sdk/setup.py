9e89870b2c8915a9bb0ffb963111cd19121c67ea
w601sxs <shreyas.f117@gmail.com>
2020-08-10 18:03:47 +0000

    changed faropt image

--- sdk/setup.py
+++ sdk/setup.py
@@ -19,7 +19,7 @@ extras = {
 }
 
 setup(name='faropt',
-      version='0.1.0',
+      version='0.1.1',
       description='Fargate based Numerical Optimization Template',
       url='https://github.com/aws-samples/faropt',
       author='Shreyas Subramanian',
