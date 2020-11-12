7dc37820d2f7a966463db6e6fbed42f0a76953d0
w601sxs <ec2-user@ip-172-16-122-172.ec2.internal>
2020-11-12 01:37:20 +0000

    updated notebook and sdk

--- sdk/build/lib/faropt/__init__.py
+++ sdk/build/lib/faropt/__init__.py
@@ -5,6 +5,9 @@ import os
 import zipfile
 from datetime import datetime
 from uuid import uuid4
+import time
+
+logging.basicConfig(level=logging.INFO)
 
 
 class FarOpt(object):
@@ -15,15 +18,17 @@ class FarOpt(object):
         try:
             response = cf.describe_stacks(StackName=stackname)
             if response['Stacks'][0]['StackStatus'] in ['CREATE_COMPLETE','UPDATE_COMPLETE']:
-                print('FarOpt backend is ready!')
+                logging.info('FarOpt backend is ready!')
                 self.ready = True
                 self.stackname = stackname
-                self.bucket = response['Stacks'][0]['Outputs'][0]['OutputValue']
+                self.bucket = response['Stacks'][0]['Outputs'][0]['OutputValue'] # S3 bucket
+                self.jobtable = response['Stacks'][0]['Outputs'][2]['OutputValue'] # DynamoDB table for jobs
+                self.recipetable = response['Stacks'][0]['Outputs'][1]['OutputValue'] # DynamoDB table for recipes
                 self.configured = False
                 self.submitted = False
         except Exception as e:
             self.ready = False
-            print(e)
+            logging.error(e)
         
         self.allowed_frameworks = ['ortools']
         
@@ -34,7 +39,7 @@ class FarOpt(object):
             self.framework = framework
     
     def configure (self,source_dir):
-        print("Listing project files ...")
+        logging.info("Listing project files ...")
         file_name = "source.zip"
         zf = zipfile.ZipFile("source.zip", "w")
     
@@ -50,18 +55,32 @@ class FarOpt(object):
         self.file_name = file_name
             
         self.configured = True
-        print("Configured job!")
+        logging.info("Configured job!")
         
     def submit(self):
         if self.configured :
-            print("Submitting job")
+            logging.info("Submitting job")
             s3_client = boto3.client('s3')
+            self.ddb_resource = boto3.resource('dynamodb')
+            self.ddb_table = self.ddb_resource.Table(self.jobtable)
+            
             try:
                 eventid = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'-'+str(uuid4())
                 response = s3_client.upload_file(self.path_file_name, self.bucket,eventid+'/'+self.file_name)
-                print(eventid)
-                print("Submitted job!")
+                
+                logging.info("Submitted job! id: " + str(eventid))
+
                 self.jobname = eventid
+                
+                # Add job to dynamoDB
+                job = {
+                'jobid': self.jobname,
+                'bucket': self.bucket,
+                'path': eventid+'/'+self.file_name}
+                
+                self.ddb_table.put_item(Item=job)
+                
+                
             except ClientError as e:
                 logging.error(e)
                 return False
@@ -70,10 +89,18 @@ class FarOpt(object):
             
         self.submitted = True
     
-    def primary_status(self):
+    def list_jobs(self, limit=10):
+        ddb_client = boto3.client('dynamodb')
         
-        return self.status()['tasks'][0]['lastStatus']
-    
+        response = ddb_client.scan(
+            TableName=self.jobtable,
+            Limit=limit)
+        
+        alljobs = []
+        for job in response['Items']:
+            alljobs.append({'jobid':job['jobid']['S'], 'bucket':job['bucket']['S'], 'path':job['path']['S']})
+            print(f"jobid:{job['jobid']['S']} | bucket:{job['bucket']['S']} | path:{job['path']['S']}")
+
     
     def stream_logs(self,start_time=0, skip=0):
         #def log_stream(client, log_group, stream_name, start_time=0, skip=0): from SM stream logs
@@ -109,25 +136,43 @@ class FarOpt(object):
                 events = []
             for ev in events:
                 yield ev
-        
+     
     
-    def logs(self, stream=False):
-        
-        if self.primary_status() in ['STOPPED','DEPROVISIONING','RUNNING']:
-            if stream == False:
-                taskarn = self.status()['tasks'][0]['taskArn'].split('/')[-1]
-                client = boto3.client('logs')
-                response = client.get_log_events(
-                            logGroupName='faroptlogGroup',
-                            logStreamName='faroptlogs/FarOptImage/' + taskarn)
+    def stop(self):
+        # if self.primary_status() in ['STOPPED','DEPROVISIONING','RUNNING']:
+        client = boto3.client('ecs')
+        taskarn = self.status()['tasks'][0]['taskArn'].split('/')[-1]
+        response = client.stop_task(
+            cluster='FarOptCluster',
+            task=taskarn,
+            reason='User stopped task'
+            )
+        # else:
+        #     logging.info('Job status: ' + self.primary_status())
             
-                print(response)
-            else:
-                for ev in self.stream_logs():
-                    print(ev)
-        else:
-            print('Job status: ' + self.primary_status()+ ' | Please wait for job to start running')
     
+    def printlogs(self,response):
+
+        for ev in response['events']:
+            print(str(ev['timestamp']) + ' | ' + ev['message'])
+    
+    def logs(self):
+
+        if self.primary_status() in ['STOPPED','DEPROVISIONING','RUNNING']:
+            taskarn = self.status()['tasks'][0]['taskArn'].split('/')[-1]
+            client = boto3.client('logs')
+            response = client.get_log_events(
+                        logGroupName='faroptlogGroup',
+                        logStreamName='faroptlogs/FarOptImage/' + taskarn)
+            
+            self.printlogs(response)
+
+        else:
+            print('Please wait for the task to start running | ' + self.primary_status())
+                
+                
+    def primary_status(self):
+        return self.status()['tasks'][0]['lastStatus']
         
     def status(self):
         if self.submitted:
@@ -140,7 +185,7 @@ class FarOpt(object):
             running_tasks = response1['taskArns']
             
             if running_tasks == []:
-                print("No running tasks. Checking completed tasks...")
+                logging.info("No running tasks. Checking completed tasks...")
                 #check if stopped tasks exist
                 response1 = client.list_tasks(
                         cluster='FarOptCluster',
--- sdk/setup.py
+++ sdk/setup.py
@@ -19,7 +19,7 @@ extras = {
 }
 
 setup(name='faropt',
-      version='0.1.2',
+      version='0.1.4',
       description='Fargate based Numerical Optimization Template',
       url='https://github.com/aws-samples/faropt',
       author='Shreyas Subramanian',
