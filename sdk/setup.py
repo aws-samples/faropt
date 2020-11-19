8ee40887d34e2b644b193fc545798bae9ab07037
EC2 Default User <ec2-user@ip-172-16-169-192.ec2.internal>
2020-11-19 01:24:59 +0000

    added return value

--- sdk/setup.py
+++ sdk/setup.py
@@ -19,7 +19,7 @@ extras = {
 }
 
 setup(name='faropt',
-      version='0.2.1',
+      version='0.2.2',
       description='Fargate based Numerical Optimization Template',
       url='https://github.com/aws-samples/faropt',
       author='Shreyas Subramanian',
