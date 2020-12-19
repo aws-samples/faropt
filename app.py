76a98417123a2eaa8d88621bfa1e78e30da1f11c
w601sxs <shreyas.f117@gmail.com>
2020-12-19 01:08:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from aws_cdk import core
 
 from faropt.faropt_stack import FaroptStack
+import os
 
 app = core.App()
-FaroptStack(app, "faropt") #,env={'region': 'us-east-2'}) #overriding region for test
+
+FaroptStack(app, "faropt", env={'region': os.environ["CDK_DEFAULT_REGION"], 'account':os.environ["CDK_DEFAULT_ACCOUNT"] }) #overriding region for test
 
 app.synth()
 
